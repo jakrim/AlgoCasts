0761b5282b4047bb2fa6b9370f39d17b8773c960
Jesse Krim <jesse.krim@gmail.com>
2020-06-05 10:48:28 -0400

    Reverse Int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let str = n.toString().split('').reverse().join('');
+
+  return parseInt(str) * Math.sign(n);
+}
+
+console.log(reverseInt(-5));
+console.log(reverseInt(500));
+console.log(reverseInt(15));
+console.log(reverseInt(-15));
+console.log(reverseInt(-90));
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//   let str = n.toString().split('').reverse().join('');
+
+//   if (n < 0) {
+//     return parseInt(str) * -1;
+//   }
+
+//   return parseInt(str);
+// }
