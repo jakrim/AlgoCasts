e3056ffa8a06285c3d3bb75830a41ec1d726c409
Jesse Krim <jesse.krim@gmail.com>
2020-06-09 11:29:57 -0400

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let chars = {};
+  let result = null;
+
+  for (let char of str) {
+    chars[char] = chars[char] + 1 || 1;
+  }
+
+  for (let key in chars) {
+    let current = chars[key];
+    if (current > result) {
+      result = key;
+    }
+  }
+  return result;
+}
 
 module.exports = maxChar;
